3170b6320cdac88c8fd6b595340ff8f31d458d7f
ryancdavey1 <ryancdavey1@gmail.com>
2020-06-10 14:56:07 -0600

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,14 +5,12 @@ require 'json'
 
 class GetRequester
 
-  attr_accessor :url
-
   def initialize(url)
     @url = url
   end
 
   def get_response_body
-    uri = URI.parse(url)
+    uri = URI.parse(@url)
     response = Net::HTTP.get_response(uri)
     response.body
   end
